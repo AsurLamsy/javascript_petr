c05728c289f556f4ccafab5f4c21f4eb738eccef
AsurMandsura <106437915+AsurMandsura@users.noreply.github.com>
2022-10-02 21:43:55 +0300

    one

---
+++ index.js
@@ -0,0 +1,14 @@
+const numberOfFilms = +prompt("Сколько всего интересного?", "");
+
+const personalDataBase = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: {},
+  privat: false,
+};
+
+const a = prompt("Какой фильм крутой?", ""),
+  b = prompt("На сколько его оцените?", ""),
+  с = prompt("Какой фильм крутой?", ""),
+  d = prompt("На сколько его оцените?", "");
