14a4376012eb7bba398bda3d345c8b347cea5dd6
AsurMandsura <106437915+AsurMandsura@users.noreply.github.com>
2022-10-04 15:26:51 +0300

    3

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalDataBase = {
@@ -8,12 +10,54 @@ const personalDataBase = {
   privat: false,
 };
 
-const a = prompt("Какой фильм был последний?", ""),
-  b = prompt("На сколько его оцените?", ""),
-  c = prompt("Какой фильм был последний?", ""),
-  d = prompt("На сколько его оцените?", "");
+// for (let i = 0; i < 2; i++) {
+//   const a = prompt("Какой фильм был последний?", ""),
+//     b = prompt("На сколько его оцените?", "");
 
-personalDataBase.movies[a] = b;
-personalDataBase.movies[c] = d;
+//   if (a != null && b != null && a != "" && (b != "") & (a.length < 50)) {
+//     personalDataBase.movies[a] = b;
+//     console.log("done");
+//   } else {
+//     console.log("error");
+//     i--;
+//   }
+// }
 
-console.log(personalDataBase);
+// while (i < 3) {
+//   const a = prompt("Какой фильм был последний?", ""),
+//     b = prompt("На сколько его оцените?", "");
+//   if (a != null && b != null && a != "" && (b != "") & (a.length < 50)) {
+//     personalDataBase.movies[a] = b;
+//     console.log("done");
+//     i++;
+//   } else {
+//     console.log("error");
+//     i--;
+//   }
+// }
+
+let i = 0;
+do {
+  const a = prompt("Какой фильм был последний?", ""),
+    b = prompt("На сколько его оцените?", "");
+  if (a != null && b != null && a != "" && (b != "") & (a.length < 50)) {
+    personalDataBase.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+  i++;
+} while (i < 2);
+
+if (personalDataBase.count < 10) {
+  console.log("Просмотрено мало фильмов");
+} else if (personalDataBase.count >= 10 && personalDataBase.count < 30) {
+  console.log("Средне");
+} else if (personalDataBase.count >= 30) {
+  console.log("Много");
+} else {
+  console.log("error");
+}
+
+// console.log(personalDataBase);
